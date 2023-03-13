2f443138878e92de08f938694e9d63c5249cdf7c
Tobin Waters <tkwaterss@gmail.com>
2023-03-13 12:01:41 -0600

    added Runtime Analysis as a PDF

--- cs/pangram.js
+++ cs/pangram.js
@@ -1,26 +1,46 @@
 let test1 = 'The quick brown fox jumps over the lazy dog!';
 let test2 = 'I like cats, but not mice';
-let test3 = 'zxcvbnmasdfghjklqwertyuiopoo'
+let test3 = 'zxcvbnmasdfghjklqwertyuiopoo';
+let test4 = 'santa cruz';
+let test5 = 'giant';
 
-const pangram = string => {
-    let key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    string = string.toLowerCase(); //O(n)
-    let count = 0;
-    for(let i = 0; i < key.length; i++) { //O(1) if alphabet is constant length, else O(n)
-        if(string.includes(key[i])) { //O(n)
-            count++
-        }
-    }
-    if(count === key.length) { //O(1)
-        return true
-    } else {
-        return false
-    }
-}
+// const formatString = string => {
+//     let stringArr = string.split(' ')
+//     let newStr = stringArr.map(word => {
+//         return word.toLowerCase().charAt(0).toUpperCase() + word.slice(1);
+//     })
+//     return newStr.join(' ')
+//  }
 
-console.log(pangram(test1))
-console.log(pangram(test2))
-console.log(pangram(test3))
+// console.log(formatString(test1))
+// console.log(formatString(test2))
+// console.log(formatString(test3))
+// console.log(formatString(test4))
+// console.log(formatString(test5))
+
+
+
+
+
+// const pangram = string => {
+//     let key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//     string = string.toLowerCase(); //O(n)
+//     let count = 0;
+//     for(let i = 0; i < key.length; i++) { //O(1) if alphabet is constant length, else O(n)
+//         if(string.includes(key[i])) { //O(n)
+//             count++
+//         }
+//     }
+//     if(count === key.length) { //O(1)
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(pangram(test1))
+// console.log(pangram(test2))
+// console.log(pangram(test3))
 
 //If we state that the alphebet length will never change
 //Runtime = O(n) - There are multiple functions that scale proportionally with the input size, but none are nested together
